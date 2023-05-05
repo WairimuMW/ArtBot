98ddf7db78c2ef29d665af0defb1c29744bb0337
WairimuMW <wachiurimary@gmail.com>
2023-05-05 13:09:33 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, jsonify
+from model import *
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import numpy as np
+import random
+
+
+# processing user input
+def bag_of_words(s, words):
+    # create an empty bag of words list
+    bag = [0 for _ in range(len(words))]
+    
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+    # generating the bag of words
+    for sent in s_words:
+        for i, w in enumerate(words):
+            if w == sent:
+                bag[i] = 1
+    
+    return np.array(bag)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/get')
+def chat():
+    while True:
+        inp = request.args.get('msg')
+        inp = inp.lower()
+        
+        # passing the processed user input to the model
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = np.argmax(results) # index of the largest value in our list
+        tag = labels[results_index]
+        
+        if results[results_index] >= 0.75:    # 75% probability
+            # getting a response from the tag
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg["responses"]
+            
+            response = random.choice(responses)
+        
+        else:
+            response = "Sorry, I don't understand. Please ask another question."
+    
+        return str(response)
+
+# run the flask app
+if __name__ == "__main__":
+	app.run()
+
---
+++ model.py
@@ -0,0 +1,155 @@
+#imports
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import numpy as np
+import tensorflow as tf
+import tflearn
+import pickle
+import json
+
+# reading the intents.json file
+with open("intents.json") as file:
+    data = json.load(file)
+
+# check if a file of the preprocessed data exists
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+
+# do this if it doesn't exist
+except:
+    # initializing empty lists
+    words = []  # list of unique words
+    labels = [] # list of unique tags
+    docs_x = [] # list of patterns
+    docs_y = [] # list of tags corresponding to the patterns in docs_x
+
+    # loading the data
+    for intent in data["intents"]:  # loop through the intents
+        for pattern in intent["patterns"]:  # loop through the patterns
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            # the tag that corresponds to the pattern
+            docs_y.append(intent["tag"])
+            
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    # stemming and vectorization
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]    # or w != "?"
+    words = sorted(list(set(words)))    # remove any duplicate elements and convert back into a list
+    labels = sorted(labels)
+
+    # creating a bag of words - one-hot encoding
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+        
+        # if the word exists we put a 1 if it doesn't we put a 0
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+
+    #converting training data into numpy arrays
+    training = np.array(training)
+    output = np.array(output)
+
+    #Saving preprocessed data
+    with open("data.pickle","wb") as f:
+        pickle.dump((words, labels, training, output), f)
+        
+
+# building the model using tflearn
+
+# getting rid of previous settings
+tf.compat.v1.reset_default_graph()
+
+# define the input shape that we're expecting for our model
+net = tflearn.input_data(shape = [None, len(training[0])])
+
+# 2 fully connected hidden layers with 8 neurons
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+
+# output layer
+# softmax function gives a probability to each neuron
+net = tflearn.fully_connected(net, len(output[0]), activation = "softmax")
+
+net = tflearn.regression(net)
+
+# to train our model
+# DNN is a type of neural network
+model = tflearn.DNN(net)
+
+# load the model if it already exists
+try:
+    model.load("model.tflearn")
+
+except:
+    # training
+    # epochs are the number of times the model will go through the same data
+    model.fit(training, output, n_epoch = 1000, batch_size = 8, show_metric = True)
+
+    # save the model
+    model.save("model.tflearn")
+
+
+
+# Using the model
+
+# processing user input
+# def bag_of_words(s, words):
+#     # create an empty bag of words list
+#     bag = [0 for _ in range(len(words))]
+    
+#     s_words = nltk.word_tokenize(s)
+#     s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+#     # generating the bag of words
+#     for sent in s_words:
+#         for i, w in enumerate(words):
+#             if w == sent:
+#                 bag[i] = 1
+    
+#     return np.array(bag)
+
+
+# Getting user input and generating responses
+# def chat():
+#     print("Start talking with the bot (type quit to end conversation)!")
+#     while True:
+#         inp = input("Artist: ")
+#         if inp.lower() == "quit":
+#             break
+        
+#         # passing the processed user input to the model
+#         results = model.predict([bag_of_words(inp, words)])[0]
+#         results_index = np.argmax(results) # index of the largest value in our list
+#         tag = labels[results_index]
+        
+#         if results[results_index] >= 0.75:    # 75% probability
+#             # getting a response from the tag
+#             for tg in data["intents"]:
+#                 if tg["tag"] == tag:
+#                     responses = tg["responses"]
+            
+#             print(random.choice(responses))
+        
+#         else:
+#             print("Sorry, I don't understand. Please ask another question.")
+
+# chat()  # calling the chat function
